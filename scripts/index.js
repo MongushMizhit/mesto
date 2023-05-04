aa7cc5cafa3df97d02e354f4418b9b85b139a0d4
Mizhit Mongush <mongushmmb@yandex.ru>
2023-05-04 12:23:59 +0700

    second review fix mistakes

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,30 @@
 const editButtonOpen = document.querySelector('.profile__button-opened'); 
 const popup = document.querySelector('.popup'); 
-const popupCloseButton = document.querySelector('.popup__close-button'); 
-const popupSubmitButton = document.querySelector('.popup__submit-button'); 
+const popupCloseButton = document.querySelector('.popup__close-button');  
 const formElement = document.querySelector('.popup__form');  
 const nameInput = document.querySelector('.popup__input_type_name'); 
 const jobInput = document.querySelector('.popup__input_type_job');
+const profileNickname = document.querySelector('.profile__nickname');
+const profileDescription = document.querySelector('.profile__description');
 
 const togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup_opened'); 
 
-editButtonOpen.addEventListener('click', () => togglePopupState(popup)); 
+editButtonOpen.addEventListener('click', () => {
+    nameInput.value = profileNickname.textContent;
+    jobInput.value = profileDescription.textContent;
+    togglePopupState(popup);
+  });
 
 popupCloseButton.addEventListener('click', () => togglePopupState(popup)); 
-
-popupSubmitButton.addEventListener('click', () => togglePopupState(popup)); 
+ 
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
   
-    document.querySelector('.profile__nickname').textContent = document.querySelector('.popup__input_type_name').value; 
-    document.querySelector('.profile__description').textContent = document.querySelector('.popup__input_type_job').value; 
+    profileNickname.textContent = nameInput.value; 
+    profileDescription.textContent = jobInput.value;
+
+    togglePopupState(popup);
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
